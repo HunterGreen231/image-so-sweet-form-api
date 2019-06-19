aa80f266904507e1c39ef026de00c7e045c6efcc
g-hunter <webdev.huntergreen@gmail.com>
2019-06-19 14:35:18 -0600

    Created database

--- app.py
+++ app.py
@@ -6,34 +6,38 @@ from flask_heroku import Heroku
 import os
 from sendgrid import SendGridAPIClient
 from sendgrid.helpers.mail import Mail
-# import config
+import config
 
 app = Flask(__name__)
 heroku = Heroku(app)
 
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgres://lwordjtgmiscda:9478bc19e2ab4f10fa3d98091018a590d455ec323d82e383f72675b7bf789821@ec2-174-129-27-158.compute-1.amazonaws.com:5432/dbr1fiolqu9b91"
+
 CORS(app)
 
-# class CustomerData(db.Model):
-#     __tablename__ = "CustomerData"
-#     id = db.Column(db.Integer, primary_key=True)
-#     customer_name = db.Column(db.String(30), nullable=False)
-#     customer_email = db.Column(db.String(50), nullable=False, unique=True)
+db = SQLAlchemy(app)
+ma = Marshmallow(app)
 
-#     def __init__(self, customer_name, customer_email):
-#         self.customer_name = customer_name
-#         self.customer_email = customer_email
+class CustomerData(db.Model):
+    __tablename__ = "CustomerData"
+    id = db.Column(db.Integer, primary_key=True)
+    customer_name = db.Column(db.String(30), nullable=False)
+    customer_email = db.Column(db.String(50), nullable=False, unique=True)
 
-# class CustomerDataSchema(ma.Schema):
-#     class Meta:
-#         fields = ("id", "customer_name", "customer_email")
+    def __init__(self, customer_name, customer_email):
+        self.customer_name = customer_name
+        self.customer_email = customer_email
+
+class CustomerDataSchema(ma.Schema):
+    class Meta:
+        fields = ("id", "customer_name", "customer_email")
 
 
-# customer_data_schema = CustomerDataSchema()
-# customers_data_schema = CustomerDataSchema(many=True)
+customer_data_schema = CustomerDataSchema()
+customers_data_schema = CustomerDataSchema(many=True)
 
 @app.route("/email-form", methods=["POST"])
 def email():
-    print(request.json)
 
     childsName = request.json["childsName"]
     email = request.json["email"]
@@ -71,7 +75,7 @@ Message: {customerMessage}<br/>
     # except Exception as e:
     #     print(e)
 
-    return "Success"
+    return content
 
 if __name__ == "__main__":
     app.debug = True
